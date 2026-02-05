3998c2a92fe12c388750707958773ce88b9f569e
Luca Gasparini <lucagasparini@Lucas-MacBook-Air.local>
2026-02-05 13:18:44 +0000

    Add homepage for Speed Insights

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Fertility Clinic Prototype</h1>
+      <p>Welcome to the site! Speed Insights should now work.</p>
+    </div>
+  );
+}
