24c9db425ed939bd138da78ed62e79a40384e1bb
Luca Gasparini <lucagasparini@Lucas-MacBook-Air.local>
2026-02-05 12:33:18 +0000

    Add _app.js with Speed Insights

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '../styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+
+export default MyApp;
+
+
+
+
